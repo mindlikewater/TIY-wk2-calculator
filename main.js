47e4e6b337a2cd6f2cc479e21ba41bc5fe4012eb
Tai Chen <mindlikewater@gmail.com>
2016-10-04 16:50:45 -0400

    Got addition function to work

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 var calcButton = document.getElementById("calculate-button");
-var answerBox = document.getElementById("answer");
 
 var runSum = function () {
-  var num1 = document.getElementById("value-one").value;
-  var num2 = document.getElementById("value-two").value;
+  var num1 = Number(document.getElementById("value-one").value);
+  var num2 = Number(document.getElementById("value-two").value);
+  var answerBox = document.getElementById("answer");
 
-  console.log(num1, num2);
-
-  return num1 + num2;
+  answerBox.innerHTML = "";
+  answerBox.innerHTML += num1 + num2;
 };
-
-console.log(runSum);
 
 calcButton.addEventListener("click", runSum);
-
-var displayAnswer = function () {
-  answerBox.innerHTML = "";
-  answerBox.innerHTML += "<p>" + runSum + "<p>";
-};

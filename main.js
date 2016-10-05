568ba64a8c927d7e68a675fa884e24fa18bf155f
Tai Chen <mindlikewater@gmail.com>
2016-10-05 10:24:05 -0400

    Changed the HTML back to form and eliminated the default event to addEvenListener

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var calcButton = document.getElementById("calculate-button");
 var runSum = function () {
   var num1 = Number(document.getElementById("value-one").value);
   var num2 = Number(document.getElementById("value-two").value);
+  event.preventDefault();
   var answerBox = document.getElementById("answer");
 
   answerBox.innerHTML = "";

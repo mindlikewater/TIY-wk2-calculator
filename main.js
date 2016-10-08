516800b02b8a9ddc03042e916e1649f54e367818
Tai Chen <mindlikewater@gmail.com>
2016-10-08 09:47:32 -0400

    Updated answerBox

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 var calcButton = document.getElementById("calculate-button");
 
-var runSum = function () {
+var runSum = function (event) {
   var num1 = Number(document.getElementById("value-one").value);
   var num2 = Number(document.getElementById("value-two").value);
   event.preventDefault();
   var answerBox = document.getElementById("answer");
 
-  answerBox.innerHTML = "";
-  answerBox.innerHTML += num1 + num2;
+  answerBox.innerHTML = num1 + num2;
 };
 
 calcButton.addEventListener("click", runSum);
